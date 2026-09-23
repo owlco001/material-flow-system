ef9bae56c892b823b2be2494e0e4a9ccb6ffdc92
owlco001 <owlco001@users.noreply.github.com>
2026-09-23 13:28:07 +0800

    test(android): update delivery version gate

--- logistics-android/app/src/test/java/com/company/logistics/VersionConfigTest.kt
+++ logistics-android/app/src/test/java/com/company/logistics/VersionConfigTest.kt
@@ -7,7 +7,7 @@ class VersionConfigTest {
 
     @Test
     fun deliveryVersionIsCurrent() {
-        assertEquals(14, BuildConfig.VERSION_CODE)
-        assertEquals("0.5.0", BuildConfig.VERSION_NAME)
+        assertEquals(15, BuildConfig.VERSION_CODE)
+        assertEquals("0.5.1", BuildConfig.VERSION_NAME)
     }
 }
